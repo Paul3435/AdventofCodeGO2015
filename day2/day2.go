6a0b98f6dfa2ef332185ba67e223189563cd28d5
Paul3435 <paul.borjesson.sesma@gmail.com>
2024-08-05 14:30:44 +0000

    day 2 part 1

--- day2/day2.go
+++ day2/day2.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,13 +19,31 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() { //Will work as long as lines are shorted than 64k bytes
-		fmt.Println(scanner.Text())
+	var totalPaper int
+
+	for scanner.Scan() { //Will work as long as lines are shorted than 64k bytes, splits based on newLines
+		stringSlice := strings.Split(scanner.Text(), "x") //make a slice using the x multiplication sign as separator
+		intSlice := make([]int, len(stringSlice))
+		for i, s := range stringSlice { //convert all strings to ints
+			intSlice[i], _ = strconv.Atoi(s)
+		}
+
+		sort.Slice(intSlice, func(i, j int) bool { //sort slice by size
+			return intSlice[i] < intSlice[j]
+		})
+
+		l := intSlice[0] //will always be the smaller
+		w := intSlice[1] //always the second smallest
+		h := intSlice[2]
+
+		totalPaper += 3*l*w + 2*w*h + 2*h*l
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(totalPaper)
 }
 
 func check(e error) {
